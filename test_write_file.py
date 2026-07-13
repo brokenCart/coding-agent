f1ef0610f6469e91dd1d1c5b1eb553234f5c776b
brokenCart <yathartgpta@gmail.com>
2026-07-13 11:55:23 +0000

    added write file function

---
+++ test_write_file.py
@@ -0,0 +1,18 @@
+from functions.write_file import write_file
+
+
+def test_write_file():
+    # Valid directory within the working directory
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print("---\n")
+
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print("---\n")
+
+    # Outside the working directory
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print("---\n")
+
+
+if __name__ == "__main__":
+    test_write_file()
